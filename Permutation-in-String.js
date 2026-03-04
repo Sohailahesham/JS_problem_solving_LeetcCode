0150ac08620dee56bf1cd9205cb2f39aaef36db5
Sohaila Hesham <95877460+Sohailahesham@users.noreply.github.com>
2026-03-04 20:43:47 +0200

    [567] [Time Beats: 31.20%] [Memory Beats: 17.52%] - LeetPush

--- Permutation-in-String.js
+++ Permutation-in-String.js
@@ -3,34 +3,41 @@
 3 * @param {string} s2
 4 * @return {boolean}
 5 */
-6var checkInclusion = function (s1, s2) {
-7  if (s1.length > s2.length) return false;
+6var checkInclusion = function(s1, s2) {
+7    if (s1.length > s2.length) return false;
 8
-9  let s1Obj = {};
-10  
-11  for (let char of s1) {
-12    s1Obj[char] = (s1Obj[char] || 0) + 1;
-13  }
-14
-15  for (let i = 0; i <= s2.length - s1.length; i++) {
-16    let txt = s2.slice(i, i + s1.length);
-17    let txtObj = {};
-18
-19    for (let char of txt) {
-20      txtObj[char] = (txtObj[char] || 0) + 1;
-21    }
-22
-23    let isMatch = true;
-24
-25    for (let key in s1Obj) {
-26      if (txtObj[key] !== s1Obj[key]) {
-27        isMatch = false;
-28        break;
-29      }
-30    }
-31
-32    if (isMatch) return true;
-33  }
+9    let s1Count = {};
+10    let windowCount = {};
+11
+12    for (let char of s1) {
+13        s1Count[char] = (s1Count[char] || 0) + 1;
+14    }
+15
+16    let windowSize = s1.length;
+17    for (let i = 0; i < windowSize; i++) {
+18        let char = s2[i];
+19        windowCount[char] = (windowCount[char] || 0) + 1;
+20    }
+21
+22    const isMatch = () => {
+23        for (let key in s1Count) {
+24            if (windowCount[key] !== s1Count[key]) return false;
+25        }
+26        return true;
+27    }
+28
+29    if (isMatch()) return true;
+30
+31    for (let i = windowSize; i < s2.length; i++) {
+32        let newChar = s2[i]; 
+33        let oldChar = s2[i - windowSize];
 34
-35  return false;
-36};
+35        windowCount[newChar] = (windowCount[newChar] || 0) + 1;
+36        windowCount[oldChar]--;
+37        if (windowCount[oldChar] === 0) delete windowCount[oldChar];
+38
+39        if (isMatch()) return true;
+40    }
+41
+42    return false;
+43};
